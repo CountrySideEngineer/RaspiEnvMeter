8414929024fee94cd5312dfbf7d8f75dbd065d73
CountrySideEngineer <deep-sleeper@nifty.com>
2020-01-02 05:27:30 +0900

    Add code to change view size depending on the platform. -If build on PC, Windows, the size of application window is fixed size. -The case build on RaspberryPi, the application shows with full screen size.

--- dev/src/RaspiEnvMeter/main.cpp
+++ dev/src/RaspiEnvMeter/main.cpp
@@ -6,6 +6,14 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     EnvMeter w;
+#if defined (BUILD_PC)
+    w.setFixedSize(480, 320);
     w.show();
+#elif defined (BUILD_RASPI)
+    w.showFullScreen();
+#else
+    w.setFixedSize(480, 320);
+    w.show();
+#endif
     return a.exec();
 }
