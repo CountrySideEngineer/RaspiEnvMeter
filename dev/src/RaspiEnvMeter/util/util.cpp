3da60d00427ac93623b5a53185779a6a1864688f
CountrySideEngineer <dep-sleeper@nifty.com>
2020-01-18 11:25:06 +0900

    Handle warning detected in compiling.

--- dev/src/RaspiEnvMeter/util/util.cpp
+++ dev/src/RaspiEnvMeter/util/util.cpp
@@ -4,7 +4,7 @@ Util::Util() { }
 
 float Util::Buff2Float(const uint8_t *buffer, int msb_index, int lsb_index, float lsb, uint16_t mask)
 {
-    uint16_t raw_data = ((uint16_t)buffer[msb_index]) << 8 + (uint16_t)buffer[lsb_index];
+    uint16_t raw_data = (((uint16_t)buffer[msb_index]) << 8) + (uint16_t)buffer[lsb_index];
     raw_data &= mask;
     float converted = (float)((int16_t)raw_data * lsb);
 
